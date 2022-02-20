aea6865bd468263a34096e7f57701df770cf4811
Abeer Naeem <abeernaeem6@gmail.com>
2022-02-20 13:52:00 -0600

    Added parts of the logic behind the game

--- script.js
+++ script.js
@@ -1,13 +1,73 @@
-
 var Rock = document.querySelector(".Rock");
 var Paper = document.querySelector(".Paper");
 var Scissor = document.querySelector(".Scissor");
+var userChoice = document.querySelector("#userChoice");
+var compChoice = document.querySelector("#compChoice");
+var winner = document.querySelector("#winner");
+var leftSection = document.querySelector("#leftSection");
+var middleSection = document.querySelector("#middleSection");
+var rightSection = document.querySelector("#rightSection");
+var decision = "";
 
 
-Rock.onclick = function() {rockClick()};
+// Computer Choice
+function random(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+var items = ["ROCK", "PAPER", "SCISSOR"];
+var computerChoice = random(items);
+
+Rock.onclick = function () {
+  rockClick()
+};
+Paper.onclick = function () {
+  paperClick()
+};
+Scissor.onclick = function () {
+  scissorClick()
+};
+
+
+// Computer Choice
+function random(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+var items = ["ROCK", "PAPER", "SCISSOR"];
+var computerChoice = random(items);
+
+
 
 function rockClick() {
+  userChoice.textContent = "You Chose";
+  leftSection.textContent = "Rock";
+  
+  compChoice.textContent = `Computer Chose ${computerChoice}`;
 
-    Rock.style.backgroundColor = "Purple";
-    
+
+  if (computerChoice === "ROCK") {
+
+    decision = "TIE"
+      
+  } else if (computerChoice === "PAPER") {
+      decision = "Computer"
+  } else {
+      decision = "You"
+  }
+
+
+  winner.textContent = `The Winner is ${decision}`;
+}
+
+function paperClick() {
+  userChoice.textContent = "You Chose";
+  leftSection.textContent = "Paper";
+  winner.textContent = "The Winner is XXXX";
+  compChoice.textContent = `Computer Chose ${computerChoice}`;
+}
+
+function scissorClick() {
+  userChoice.textContent = "You Chose";
+  leftSection.textContent = "Scissor";
+  winner.textContent = "The Winner is XXXX";
+  compChoice.textContent = `Computer Chose ${computerChoice}`;
 }
