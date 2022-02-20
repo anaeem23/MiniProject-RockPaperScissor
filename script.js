00420f1389bb1e1a4d738611690249677bb6df95
Abeer Naeem <abeernaeem6@gmail.com>
2022-02-20 13:16:35 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var Rock = document.querySelector(".Rock");
+var Paper = document.querySelector(".Paper");
+var Scissor = document.querySelector(".Scissor");
+
+
+Rock.onclick = function() {rockClick()};
+
+function rockClick() {
+
+    Rock.style.backgroundColor = "Purple";
+    
+}
